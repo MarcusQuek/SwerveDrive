b4cf4834629a07735bf01c09a6715c027d6da5bf
John Chai <jc050402@gmail.com>
2024-10-23 19:43:46 +0800

    synced definitions.h

--- include/definitions.h
+++ include/definitions.h
@@ -7,26 +7,56 @@
 #include <vector>
 #include "api.h"
 
-#define LEFT_UPPER_BEVEL_MOTOR_1 15
-#define LEFT_UPPER_BEVEL_MOTOR_2 12 
-#define LEFT_LOWER_BEVEL_MOTOR_1 1
-#define LEFT_LOWER_BEVEL_MOTOR_2 2
-#define RIGHT_UPPER_BEVEL_MOTOR_1 20
-#define RIGHT_UPPER_BEVEL_MOTOR_2 19
-#define RIGHT_LOWER_BEVEL_MOTOR_1 9
-#define RIGHT_LOWER_BEVEL_MOTOR_2 7
+// #define LEFT_UPPER_BEVEL_MOTOR_1 15
+// #define LEFT_UPPER_BEVEL_MOTOR_2 12 
+// #define LEFT_LOWER_BEVEL_MOTOR_1 1
+// #define LEFT_LOWER_BEVEL_MOTOR_2 2
+// #define RIGHT_UPPER_BEVEL_MOTOR_1 20
+// #define RIGHT_UPPER_BEVEL_MOTOR_2 19
+// #define RIGHT_LOWER_BEVEL_MOTOR_1 9
+// #define RIGHT_LOWER_BEVEL_MOTOR_2 7
 
-#define LEFT_LIFT_MOTOR 13
-#define RIGHT_LIFT_MOTOR 18
+// #define LEFT_UPPER_BEVEL_MOTOR_1 11
+// #define LEFT_UPPER_BEVEL_MOTOR_2 12
+// #define LEFT_LOWER_BEVEL_MOTOR_1 19
+// #define LEFT_LOWER_BEVEL_MOTOR_2 20
+// #define RIGHT_UPPER_BEVEL_MOTOR_1 1
+// #define RIGHT_UPPER_BEVEL_MOTOR_2 2
+// #define RIGHT_LOWER_BEVEL_MOTOR_1 9
+// #define RIGHT_LOWER_BEVEL_MOTOR_2 10
 
-#define UPPER_INTAKE_MOTOR 5
-#define LOWER_INTAKE_MOTOR 4
+// #define LEFT_LIFT_MOTOR 18
+// #define RIGHT_LIFT_MOTOR 8
+
+// #define UPPER_INTAKE_MOTOR 15
+// #define LOWER_INTAKE_MOTOR 14
+
+// #define IMU_SENSOR_PORT 16
+// #define SERIALPORT 17
+
+// #define LEFT_ROTATION_SENSOR_PORT 13
+// #define RIGHT_ROTATION_SENSOR_PORT 3
+
+#define LEFT_UPPER_BEVEL_MOTOR_1 11
+#define LEFT_UPPER_BEVEL_MOTOR_2 12
+#define LEFT_LOWER_BEVEL_MOTOR_1 19
+#define LEFT_LOWER_BEVEL_MOTOR_2 20
+#define RIGHT_UPPER_BEVEL_MOTOR_1 2
+#define RIGHT_UPPER_BEVEL_MOTOR_2 3
+#define RIGHT_LOWER_BEVEL_MOTOR_1 8
+#define RIGHT_LOWER_BEVEL_MOTOR_2 9
+
+#define LEFT_LIFT_MOTOR 18
+#define RIGHT_LIFT_MOTOR 10
+
+#define UPPER_INTAKE_MOTOR 15
+#define LOWER_INTAKE_MOTOR 14
 
 #define IMU_SENSOR_PORT 17
-#define SERIALPORT 14
+#define SERIALPORT 16
 
-#define LEFT_ROTATION_SENSOR_PORT 6
-#define RIGHT_ROTATION_SENSOR_PORT 8
+#define LEFT_ROTATION_SENSOR_PORT 13
+#define RIGHT_ROTATION_SENSOR_PORT 1
 
 #define POTENTIOMETER_SENSOR_PORT 'H'
 #define SOLENOID_SENSOR_PORT 'G'
@@ -65,8 +95,8 @@ extern "C" int32_t vexGenericSerialTransmit( uint32_t index, uint8_t *buffer, in
 
 //Controllers
 int leftX = 0, leftY = 0, rightX = 0;
-const double DEADBAND = 15.0;
-const double MAX_RPM = 600.0;
+const double DEADBAND = 8.0;
+const double MAX_RPM = 450.0;
 const double SCALING_FACTOR = MAX_RPM / 127.0;
 const double TO_DEGREES = (180.0 / M_PI);
 const double TO_RADIANS = (M_PI / 180.0);
@@ -76,15 +106,16 @@ const double WHEEL_BASE_RADIUS = 0.2;
 double left_wheel_speed = 0.0;
 double right_wheel_speed = 0.0;
 
-
 //SetWheelAngle
-const double lkP = 1.8;
-const double lkI = 0.0;
-const double lkD = 0.09;
+bool setAngle = false;
 
-const double rkP = 1.8;
+const double lkP = 1.1;
+const double lkI = 0.0;
+const double lkD = 0.0;
+
+const double rkP = 1.1;
 const double rkI = 0.0;
-const double rkD = 0.09;
+const double rkD = 0.0;
 
 double target_angle = 0.0;
 double target_angleL = 0.0;
@@ -92,6 +123,8 @@ double target_angleR = 0.0;
 double left_turn_speed = 0.0;
 double right_turn_speed = 0.0;
 double rotational = 0.0;
+double rotationalL = 0.0;
+double rotationalR = 0.0;
 
 //MogoLift
 const double mkP = 0.88;
@@ -103,3 +136,9 @@ bool liftEnable = false;
 
 bool isLeftFlipped = false;
 bool isRightFlipped = false;
+
+//Serial
+double global_distY = 0.0;
+double global_distX = 0.0;
+double global_errorY = 0.0;
+double global_errorX = 0.0;
